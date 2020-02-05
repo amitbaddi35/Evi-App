7f9053ec6f4130672c0571ebd796e1b805500c41
AMIT <amitbaddi35@gmail.com>
2020-02-05 20:16:15 +0530

    05-02-2020

--- app/src/main/java/com/smsoft/evischoolmanagementapp/MainActivity.java
+++ app/src/main/java/com/smsoft/evischoolmanagementapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.smsoft.evischoolmanagementapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -11,185 +10,66 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+
 import com.smsoft.evischoolmanagementapp.PoJo.loginPoJo;
 import com.smsoft.evischoolmanagementapp.SharedPref.StudSharedPref;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+    loginPoJo.Stud_Data stud_data;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-
-    public  static final int RequestPermissionCode  = 1 ;
-    Button buttonEnable, buttonGet ;
-    TextView textViewLongitude, textViewLatitude ;
-    Context context;
-    Intent intent1 ;
-    Location location;
-    LocationManager locationManager ;
-    boolean GpsStatus = false ;
-    Criteria criteria ;
-    String Holder;
-
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EnableRuntimePermission();
+        new Handler().postDelayed(new Runnable() {
 
-        buttonEnable = (Button)findViewById(R.id.button);
-        buttonGet = (Button)findViewById(R.id.button2);
-        textViewLongitude = (TextView)findViewById(R.id.textView);
-        textViewLatitude = (TextView)findViewById(R.id.textView2);
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        criteria = new Criteria();
-        Holder = locationManager.getBestProvider(criteria, false);
-        context = getApplicationContext();
-        CheckGpsStatus();
-
-        buttonEnable.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
-            public void onClick(View v) {
-                intent1 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent1);
+            public void run() {
+                Intent HomeIntent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(HomeIntent);
+                StudSharedPref s = new StudSharedPref(MainActivity.this);
+
+
+                /*    U ll get Firebase ID here  */
+                /*FirebaseInstanceId.getInstance().getInstanceId()
+                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.w("token", "getInstanceId failed", task.getException());
+                                    return;
+                                }
+
+                                // Get new Instance ID token
+                                String token = task.getResult().getToken();
+
+                                // Log and toast
+                                //  String msg = getString(R.string.msg_token_fmt, token);
+                                Log.d("token", token);
+                                Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                            }
+                        });*/
+
+               /* WifiManager manager = (WifiManager) getApplicationContext().getSystemService(MainActivity.this.WIFI_SERVICE);
+                WifiInfo info = manager.getConnectionInfo();
+                String address = info.getMacAddress();
+                Log.d("Trace",address);
+               finish();  */
+
+                //myDialog.dismiss();
             }
-        });
-
-        buttonGet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                CheckGpsStatus();
-
-                if(GpsStatus == true) {
-                    if (Holder != null) {
-                        if (ActivityCompat.checkSelfPermission(
-                                MainActivity.this,
-                                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                                &&
-                                ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                                        != PackageManager.PERMISSION_GRANTED) {
-                            return;
-                        }
-                        location = locationManager.getLastKnownLocation(Holder);
-                        locationManager.requestLocationUpdates(Holder, 12000, 7, MainActivity.this);
-                    }
-
-                }else {
-
-                    Toast.makeText(MainActivity.this, "Please Enable GPS First", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
-
-
+        },SPLASH_TIME_OUT);
     }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        textViewLongitude.setText("Longitude:" + location.getLongitude());
-        textViewLatitude.setText("Latitude:" + location.getLatitude());
-        /*------- To get city name from coordinates --------*/
-        String cityName = null;
-        Geocoder gcd = new Geocoder(MainActivity.this, Locale.getDefault());
-        List<Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(location.getLatitude(),
-                    location.getLongitude(), 1);
-            if (addresses.size() > 0) {
-                System.out.println(addresses.get(0).getLocality());
-                cityName = addresses.get(0).getLocality();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        String s = location.getLongitude() + "\n" + location.getLatitude() + "\n\nMy Current City is: "
-                + cityName;
-        Log.d("trace",s);
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    public void CheckGpsStatus(){
-
-        locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
-        GpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-    }
-
-    public void EnableRuntimePermission(){
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION))
-        {
-
-            Toast.makeText(MainActivity.this,"ACCESS_FINE_LOCATION permission allows us to Access GPS in app", Toast.LENGTH_LONG).show();
-
-        } else {
-
-
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION}, RequestPermissionCode);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
-
-        switch (RC) {
-
-            case RequestPermissionCode:
-
-                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Toast.makeText(MainActivity.this,"Permission Granted, Now your application can access GPS.", Toast.LENGTH_LONG).show();
-
-                } else {
-
-                    Toast.makeText(MainActivity.this,"Permission Canceled, Now your application cannot access GPS.", Toast.LENGTH_LONG).show();
-
-                }
-                break;
-        }
-    }
-
-
 }
